264d582d0ecdebeeead98b6e345462b8d172f283
Matik Zed <matikzed@gmail.com>
2022-05-06 22:47:54 -0400

    adds sessions based on a uuid and the ability to play again

--- public/index.js
+++ public/index.js
@@ -1,15 +1,29 @@
+const guessOneInput = document.querySelector('#guessOne');
+const guessTwoInput = document.querySelector('#guessTwo');
+const guessThreeInput = document.querySelector('#guessThree');
+const guessFourInput = document.querySelector('#guessFour');
+
+const roomId = window.location.pathname.split('/')[2];
 const submitBtn = document.querySelector('#submit');
+const guessDiv = document.querySelector('#guesses');
+
+function addPlayAgain() {
+    //disable the submit button
+    submitBtn.disabled = true;
+    // add a way to play again
+    const playAgainLink = document.createElement('a');
+    playAgainLink.innerText = 'Play Again';
+    playAgainLink.setAttribute('href', '/');
+    guessDiv.appendChild(playAgainLink);
+}
 
 submitBtn.addEventListener('click', () => {
-    const guessOne = document.querySelector('#guessOne').value;
-    const guessTwo = document.querySelector('#guessTwo').value;
-    const guessThree = document.querySelector('#guessThree').value;
-    const guessFour = document.querySelector('#guessFour').value;
+    const guessOne = guessOneInput.value;
+    const guessTwo = guessTwoInput.value;
+    const guessThree = guessThreeInput.value;
+    const guessFour = guessFourInput.value;
 
-    const guessDiv = document.querySelector('#guesses');
-
-    let url = `/guess?guessOne=${guessOne}&guessTwo=${guessTwo}&guessThree=${guessThree}&guessFour=${guessFour}`
-
+    let url = `/guess/${roomId}/?guessOne=${guessOne}&guessTwo=${guessTwo}&guessThree=${guessThree}&guessFour=${guessFour}`
 
     fetch(url).then(res => {
         return res.text();
@@ -17,10 +31,23 @@ submitBtn.addEventListener('click', () => {
         //update the webpage with the guess here
         let newPTag = document.createElement('p');
         const [rightNumbers, rightPosition] = text.split(' ');
+
         guessResults = `Your guess was: ${guessOne} ${guessTwo} ${guessThree} ${guessFour}. `; 
         guessResults += `You guessed ${rightNumbers} right numbers. ${rightPosition} of those numbers are in their correct position.`;
+
         newPTag.innerText = guessResults;
         guessDiv.appendChild(newPTag);
+        // the player has won!
+        if(rightPosition === '4') {
+            //tell the user they won
+            alert('You won!');
+            addPlayAgain();
+        }
+        const guessDivChildrenCount = guessDiv.getElementsByTagName('*').length;
+        if(guessDivChildrenCount === 10) {
+            alert('You lost. Try again!');
+            addPlayAgain();
+        }
     });
 });
 
--- server.js
+++ server.js
@@ -1,32 +1,101 @@
-const StatsD = require('hot-shots');
-const dogstatsd = new StatsD;
-
-
-
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const crypto = require("crypto");
+
+const StatsD = require('hot-shots');
+const dogstatsd = new StatsD;
 
 const port = process.env.port || 3000;
-let randomNumbers;
+
+// a regular expression that catches a uuid
+const matchRoomUrl = /^\/room\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+const matchGuessUrl = /^\/guess\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+
+const baseUrl = "https://www.random.org/integers/";
+
+const params = {
+    num: 4,
+    min: 0,
+    max: 7,
+    col: 1,
+    base: 10,
+    format: 'plain',
+    rnd: 'new'
+}
+
+function buildUrl(baseUrl, params) {
+    let paramsStr = "";
+    for(const [param, paramValue] of Object.entries(params)) {
+        paramsStr += param + '=' + paramValue + '&';    
+    }
+    return baseUrl + "?" + paramsStr;
+}
+
+const uuidToGeneratedNumber = new Map();
+
 
 const server = http.createServer((req, res) => {
+    console.log(req.url);
+
     if(req.method !== 'GET') {
         res.end(`{"error": "${http.STATUS_CODES[405]}"}`);
-    } else if(req.url === "/"){
-
-        fs.readFile("./public/index.html", "UTF-8", (err, html) => {
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.end(html);
-        });
+    } else if(req.url === "/") {
+        const uuid = crypto.randomUUID();
+        // redirect to a room 
+        res.writeHead(302, {
+            location: `/room/${uuid}`
+        }).end();
         dogstatsd.increment('page.views');
-    } else if(req.url === "/index.js") {
+    } else if(matchRoomUrl.test(req.url)) {
+        let uuid = req.url.split('/')[2]; 
+        // if we don't have random numbers generated for this UUID then fetch some
+        // otherwise reuse what is in the map object
+        if(uuidToGeneratedNumber.has(uuid)) {
+            fs.readFile("./public/index.html", "UTF-8", (err, html) => {
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.end(html);
+            });
+        } else {
+
+            https.get(buildUrl(baseUrl, params), (resp) => {
+                let data = '';
+
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                resp.on('end', () => {
+                    let numberStr = "";
+                    // remove new lines
+                    for(const letter of data) {
+                        if(letter !== '\n') {
+                            numberStr += letter;
+                        }
+                    }
+                    // split the string of numbers into an array
+                    randomNumbers = numberStr.split("").map(num => Number(num));
+                    console.log(randomNumbers)
+                    uuidToGeneratedNumber.set(uuid, randomNumbers);
+                });
+            });
+
+            fs.readFile("./public/index.html", "UTF-8", (err, html) => {
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.end(html);
+            });
+        }
+    } else if(req.url === "/room/index.js") {
         const filestream = fs.createReadStream("./public/index.js", "UTF-8");
         res.writeHead(200, {"Content-Type": "text/javascript"});
         filestream.pipe(res);
-
-    } else if(req.url.includes("/guess")) {
+    } else if(req.url.includes('/guess')) {
+        const uuid = req.url.split('/')[2];
+        // if the random numbers are not here we need to regenerate random numbers
+        const randomNumbers = uuidToGeneratedNumber.get(uuid) || [8, 8, 8, 8];  
+        console.log('the uuid is:', uuid, 'and the random numbes generated for this uuid is', randomNumbers);
+        
         userGuesses = req.url.split('?')[1].split('&');
         guesses = [];
         guessesInDict = {};
@@ -50,7 +119,8 @@ const server = http.createServer((req, res) => {
         for(let i = 0; i < randomNumbers.length; i++) {
             if(randomNumbers[i] === guesses[i]) correctNumbersInPosition++;
         }
-
+        // deleting the uuid from the map object will force a new set of random numbers
+        if(correctNumbersInPosition === 4) uuidToGeneratedNumber.delete(uuid);
 
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(correctNumbersInGuess + " " + correctNumbersInPosition);
@@ -65,44 +135,3 @@ server.listen(port, () => {
 });
 
 
-let baseUrl = "https://www.random.org/integers/";
-
-const params = {
-    num: 4,
-    min: 0,
-    max: 7,
-    col: 1,
-    base: 10,
-    format: 'plain',
-    rnd: 'new'
-}
-
-function buildUrl(baseUrl, params) {
-    let paramsStr = "";
-    for(const [param, paramValue] of Object.entries(params)) {
-        paramsStr += param + '=' + paramValue + '&';    
-    }
-    return baseUrl + "?" + paramsStr;
-}
-
-
-https.get(buildUrl(baseUrl, params), (resp) => {
-    let data = '';
-
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
-
-    resp.on('end', () => {
-        let numberStr = "";
-        // remove new lines
-        for(const letter of data) {
-            if(letter !== '\n') {
-                numberStr += letter;
-            }
-        }
-        // split the string of numbers into an array
-        randomNumbers = numberStr.split("").map(num => Number(num));
-        console.log(randomNumbers);
-    });
-});
