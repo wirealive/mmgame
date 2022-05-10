f289c0ba6f771a75026ca095ec252b1468db773e
Matik Zed <matikzed@gmail.com>
2022-05-10 13:56:55 -0400

    refactors server side routes code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
 const crypto = require("crypto");
 
 const StatsD = require('hot-shots');
@@ -11,7 +10,6 @@ const port = process.env.port || 3000;
 
 // a regular expression that catches a uuid
 const matchRoomUrl = /^\/room\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
-const matchGuessUrl = /^\/guess\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
 
 const baseUrl = "https://www.random.org/integers/";
 
@@ -35,69 +33,90 @@ function buildUrl(baseUrl, params) {
 
 const uuidToGeneratedNumber = new Map();
 
-
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-
-    if(req.method !== 'GET') {
-        res.end(`{"error": "${http.STATUS_CODES[405]}"}`);
-    } else if(req.url === "/") {
+function handleRootUrl(req, res) {
         const uuid = crypto.randomUUID();
         // redirect to a room 
         res.writeHead(302, {
             location: `/room/${uuid}`
         }).end();
         dogstatsd.increment('page.views');
-    } else if(matchRoomUrl.test(req.url)) {
-        let uuid = req.url.split('/')[2]; 
-        // if we don't have random numbers generated for this UUID then fetch some
-        // otherwise reuse what is in the map object
-        if(uuidToGeneratedNumber.has(uuid)) {
-            fs.readFile("./public/index.html", "UTF-8", (err, html) => {
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.end(html);
+}
+
+function handleRoomUrl(req, res) {
+    // getting the uuid like this might break the api
+    let uuid = req.url.split('/')[2]; 
+    console.log(uuid);
+
+    // if we don't have random numbers generated for this UUID then fetch some
+    // otherwise reuse what is in the map object
+    if(!uuidToGeneratedNumber.has(uuid)) {
+        console.log('getting random numbers from random.org');
+        // get random numbers from random.org api
+        const url = buildUrl(baseUrl, params);
+        console.log(url);
+        https.get(url, (res) => {
+            const { statusCode } = res
+
+            let error;
+
+            if (statusCode !== 200) {
+                error = new Error(`Request Failed. Status Code: ${statusCode}`);
+            }            
+
+            if (error) {
+                // send error to data dog here
+                console.error(error.message);
+                return;
+            }
+
+            let rawData = '';
+
+
+            res.on('data', (chunk) => {
+                console.log(chunk);
+                rawData += chunk;
             });
-        } else {
-            https.get(buildUrl(baseUrl, params), (resp) => {
-                let data = '';
 
-                resp.on('data', (chunk) => {
-                    data += chunk;
-                });
-
-                resp.on('end', () => {
-                    let numberStr = "";
-                    // remove new lines
-                    for(const letter of data) {
-                        if(letter !== '\n') {
-                            numberStr += letter;
-                        }
-                    }
+            res.on('end', () => {
+                try {
+                    // remove white space from string
+                    const randNumbersStr = rawData.replace(/\s+/g, "");
+                    console.log(rawData);
+                    console.log(randNumbersStr);
+                    
                     // split the string of numbers into an array
-                    randomNumbers = numberStr.split("").map(num => Number(num));
+                    const randomNumbers = randNumbersStr.split("").map(num => Number(num));
+                    console.log(randomNumbers);
                     uuidToGeneratedNumber.set(uuid, [randomNumbers, 0]);
-                });
+                } catch (e) {
+                    console.error('There was an error parsing the response from the API');
+                    console.error(e.message);
+                    // log to datadog here
+                }
             });
+        }).on('error', (e) => {
+            // send any errors here to datadog
+            console.error(`Got error: ${e.message}`);
+        });
+    }
 
-            fs.readFile("./public/index.html", "UTF-8", (err, html) => {
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.end(html);
-            });
-        }
-    } else if(req.url === "/room/index.js") {
-        const filestream = fs.createReadStream("./public/index.js", "UTF-8");
-        res.writeHead(200, {"Content-Type": "text/javascript"});
-        filestream.pipe(res);
-    } else if(req.url.includes('/guess')) {
-        const uuid = req.url.split('/')[2];
-        // if the random numbers are not here we need to regenerate random numbers
-        const guessInfo = uuidToGeneratedNumber.get(uuid) || [[NaN, NaN, NaN, NaN], Infinity];  
+    // serve static index html file
+    fs.readFile("./public/index.html", "UTF-8", (err, html) => {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(html);
+    });
+}
+
+
+function handleGuessUrl(req, res) {
+    const uuid = req.url.split('/')[2];
+    // What should we do if we don't have a set of random Numbers generated for this room url?
+    try {
+        const guessInfo = uuidToGeneratedNumber.get(uuid);  
         console.log(guessInfo);
         const randomNumbers = guessInfo[0];
         let attempts = guessInfo[1];
 
-        console.log('the uuid is:', uuid, 'and the random numbes generated for this uuid is', randomNumbers);
-        
         userGuesses = req.url.split('?')[1].split('&');
         guesses = [];
         guessesInDict = {};
@@ -110,34 +129,66 @@ const server = http.createServer((req, res) => {
                 guessesInDict[numberGuess] = 1;
             }
         }
-        let correctNumbersInGuess = 0;
-        let correctNumbersInPosition = 0;
 
-        for(let x of randomNumbers) {
-            if(x in guessesInDict) {
-                correctNumbersInGuess++;
+        let correctNumsInPosition = 0;
+        let correctNumsNotInPosition = 0;
+
+        for(let i = 0; i < randomNumbers.length; i++) {
+            if(randomNumbers[i] === guesses[i]) {
+                correctNumsInPosition++;
+                // mark this number as counted so that we don't double count
+                guesses[i] = 'x';
             }
         }
+
         for(let i = 0; i < randomNumbers.length; i++) {
-            if(randomNumbers[i] === guesses[i]) correctNumbersInPosition++;
+            for(let j = 0; j < guesses.length; j++) {
+                if(randomNumbers[i] === guesses[j]) correctNumsNotInPosition++;
+            }
         }
-        // deleting the uuid from the map object will force a new set of random numbers
-        if(correctNumbersInPosition === 4 || attempts === 10) {
-            uuidToGeneratedNumber.delete(uuid);
+
+        if(correctNumsInPosition === 4) {
+            console.log('user already won');    
+        } else if (attempts === 10) {
+            // send back an unauthorized request 
+            console.log('user has already attempted at least 10 times');
         } else {
             uuidToGeneratedNumber.set(uuid, [randomNumbers, ++attempts]);
         }
+        console.log('the uuid is:', uuid, 'and the random numbes generated for this uuid is', randomNumbers);
 
         res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(correctNumbersInGuess + " " + correctNumbersInPosition);
+        res.end(correctNumsNotInPosition + " " + correctNumsInPosition);
+    } catch (err) {
+        console.error(err);
+        res.end("0 0");
+    }
+}
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    if(req.method !== 'GET') {
+        // we don't want to handle any other HTTP method besides GET
+        res.end(`{"error": "${http.STATUS_CODES[405]}"}`);
+    } else if(req.url === "/") {
+        handleRootUrl(req, res);
+    } else if(matchRoomUrl.test(req.url)) {
+        handleRoomUrl(req, res);
+    } else if(req.url === "/room/index.js") {
+        // serving static js client code
+        // consider serving this fron Nginx and logging at that part of the stack
+        const filestream = fs.createReadStream("./public/index.js", "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/javascript"});
+        filestream.pipe(res);
+    } else if(req.url.includes('/guess')) {
+        handleGuessUrl(req, res);
     } else {
         res.writeHead(404, {"Content-Type": "text/html"});
         res.end("No Page Found");
+        // log the url to datadog
     }
 });
 
 server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
-
-
